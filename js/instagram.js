587a1c82f3243ce374c5c5fef21f10599e289b75
chuangwailinjie <601397895@qq.com>
2015-12-17 19:17:00 +0800

    Site updated: 2015-12-17 19:16:57

--- js/instagram.js
+++ js/instagram.js
@@ -32,11 +32,14 @@ define([], function (){
 		
 		var content;
 		
-		$.getJSON('edit.json').success(function(data)
-		{
-			content=data;
-		});
-		
+		$.ajax({  
+            url:'edit.json',  
+            async:false,
+            success:function(data){
+            	content=data;
+            }  
+        });  
+
 		return content;
 	}
 	
@@ -64,20 +67,7 @@ define([], function (){
 	}
 	
 	var getList = function(){
-		/*var photoData = [{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai1.png","text":"上海外滩","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai2.png","text":"我-背影","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai3.png","text":"我与马赛克","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai4.png","text":"未名桥","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai5.png","text":"色彩斑斓","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai6.png","text":"随手拍","y":2014,"m":6},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai7.png","text":"宜家","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shanghai8.png","text":"被抓拍","y":2015,"m":10},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/shaonv.png","text":"很像","y":2015,"m":12},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/renshenghaihai.jpg","text":"人生海海","y":2014,"m":11},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/giveup.jpg","text":"你知道的-不放弃","y":2015,"m":12},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/eo.jpg","text":"理想型、很像","y":2015,"m":11},
-{"src":"http://7xowaa.com1.z0.glb.clouddn.com/asinecat.jpg","text":"ashin-瞄","y":2013,"m":7}];*/
-
+		
 		var imgData=getImgData();
 		
 		ctrler(imgData);
@@ -88,7 +78,7 @@ define([], function (){
 			$(".img-box").css({"width":"auto", "height":"auto"});
 		}else{
 			var width = $(".img-box-ul").width();
-			var size = Math.max(width*0.26, 155);   //var size = Math.max(width*0.26, 157);
+			var size = Math.max(width*0.26, 155);   
 			$(".img-box").width(size).height(size);
 		}
 		$('.instagram img').each(function() {
